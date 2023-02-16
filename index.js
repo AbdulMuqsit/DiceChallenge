c379876c5c53e41630fbf135a9c9a1b03999cd21
Abdul Muqsit Zargar <96869359+AbdulMuqsit@users.noreply.github.com>
2023-02-16 14:49:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
 // console.log(randomNumber);
 var randomDiceImage="dice"+ randomNumber1 +".png";
-var randomImageSource="images/" + randomDiceImage;
+var randomImageSource=randomDiceImage;
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource);
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImageSource2="images/dice"+randomNumber2+".png";
+var randomImageSource2="dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 if(randomNumber1>randomNumber2)
 {
